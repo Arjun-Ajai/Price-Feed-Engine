9d840a1b8411fb28c96b967dede0f9c53fb08cc0
ARJUN AJAI <pkd25cs015@gecskp.ac.in>
2026-09-03 23:08:01 +0530

    Add command parsing and list functionality

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ def cmd_list(db: Database):
     for s in symbols:
         table.add_row(s["ticker"], str(s["bar_count"]), s["from_date"], s["to_date"])
     Console().print(table)
+        p_list = sub.add_parser("list")
+
+    args = parser.parse_args()
+    db = Database()
+
+    if args.command == "fetch":
+        cmd_fetch(args, db)
+    elif args.command == "list":
+        cmd_list(db)
+    else:
+        parser.print_help()
